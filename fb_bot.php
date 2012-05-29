8e1f7b71fd6fb2ee887c9cf55242f4de6e9bbe96
noob <noob@188.230.168.51>
2012-05-29 04:56:26 +0200

    Full code rework. + Added location check and verify checkpoint bypass. + Used DOM instead of regex, it is more reliable. + More functions, so it will be easier to implement new features.

--- fb_bot.php
+++ fb_bot.php
@@ -3,190 +3,249 @@
 /**
  * PHP Curl status update script
  * @since Sep 2010
- * @version 1.0
+ * @version 29.5.2012
  * @link http://360percents.com/posts/php-curl-status-update-working-example/
  * @author Luka Pušić <pusic93@gmail.com>
  */
-
-/**
+/*
  * Required parameters
  */
-$status = 'It Works :)';
+$status = 'Your status.';
 $email = 'your@email.com';
-$pass = 'your password';
-
-/**
- * Call the update function with your email, password and status parameters.
+$pass = 'your_password';
+/*
+ * Optional parameters
  */
-update(urlencode($email), urlencode($pass), $status);
+$uagent = 'Mozilla/4.0 (compatible; MSIE 5.0; S60/3.0 NokiaN73-1/2.0(2.0617.0.0.7) Profile/MIDP-2.0 Configuration/CLDC-1.1)';
+$cookies = 'cookies.txt';
+touch($cookies);
+$device_name = 'Home'; #in case you have location checking turned on
+$debug = false;
 
-function update($email, $pass, $status) {
-    /**
-     * Optional parameters
-     */
-    $uagent = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)';
-    $cookies = 'cookie.txt';
+/*
+ * @return form input field names & values
+ */
 
-    /**
-     * prepare status (Facebook allows only 420 chars)
-     */
-    if (strlen($status) > 419) {
-	$status = urlencode(substr($status, 0, 419));
-	echo "[*] Status was stripped to 420 characters!\n\n";
-    } else {
-	$status = urlencode($status);
-    }
-
-    function postify($arr) {
-	$fields_string = '';
-	foreach ($arr as $key => $value) {
-	    $fields_string .= $key . '=' . $value . '&';
-	}
-	return rtrim($fields_string, '&');
-    }
-
-    /**
-     * GET: http://m.facebook.com/
-     * Parse the webpage and collect login parameters
-     * (chartest, lsd, formaction).
-     */
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_COOKIEJAR, $cookies);
-    curl_setopt($ch, CURLOPT_USERAGENT, $uagent);
-    curl_setopt($ch, CURLOPT_URL, "http://m.facebook.com/");
-    curl_setopt($ch, CURLOPT_COOKIEJAR, $cookies);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-    $buf = curl_exec($ch);
-
-    echo "\n[+] Sending GET request to: http://m.facebook.com/\n\n";
-
-    preg_match("/<form method=\"post\" class=\"mobile-login-form\" id=\"login_form\" action=\"(.*)\"/U", $buf, $formaction);
-    urlencode(preg_match("/<input type=\"hidden\" name=\"charset_test\" value=\"(.*)\" \/>/U", $buf, $chartest));
-    //preg_match("/<input type=\"hidden\" name=\"lsd\" value=\"(.*)\" autocomplete=\"off\" \/>/U", $buf, $lsd);
-    preg_match("/<input type=\"hidden\" name=\"post_form_id\" value=\"(.*)\" \/>/U", $buf, $formid);
-
-    curl_close($ch);
-    unset($buf);
-    unset($ch);
-
-    /**
-     * LOGIN: POST to facebook login form (formaction) with previously
-     * collected parameters lsd, chartest, email, pass and login.
-     */
-    $postdata = array(
-	'lsd' => '',
-	'post_form_id' => $formid[1],
-	'charset_test' => $chartest[1],
-	'email' => $email,
-	'pass' => $pass,
-	'ajax' => '0',
-	'width' => '0',
-	'pxr' => '0',
-	'gps' => '0',
-	'version' => '1',
-	'login' => 'Log+in'
-    );
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_COOKIEFILE, $cookies);
-    curl_setopt($ch, CURLOPT_COOKIEJAR, $cookies);
-    curl_setopt($ch, CURLOPT_USERAGENT, $uagent);
-    curl_setopt($ch, CURLOPT_URL, $formaction[1]);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, postify($postdata));
-
-    $buf = curl_exec($ch);
-    echo $buf;
-
-    echo "[+] Sending POST data to: " . html_entity_decode(urldecode($formaction[1])) . "\n";
-    print_r($postdata);
-    echo "\n\n";
-
-    curl_close($ch);
-    unset($buf);
-    unset($ch);
-    unset($postdata);
-    /**
-     * Fetch facebook.com/home.php to Collect parameters from the form which
-     * updates status (formaction, chartest, dtsg, formid). This step is
-     * important because we want to avoid using CURLOPT_FOLLOWLOCATION.
-     */
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_COOKIEFILE, $cookies);
-    curl_setopt($ch, CURLOPT_COOKIEJAR, $cookies);
-    curl_setopt($ch, CURLOPT_USERAGENT, $uagent);
-    curl_setopt($ch, CURLOPT_URL, 'http://m.facebook.com/home.php');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-    $buf = curl_exec($ch); //execute the curl command
-
-    echo "[+] Fetching http://m.facebook.com/home.php\n\n";
-
-    preg_match("/<form method=\"post\" id=\"composer_form\" action=\"\/a\/home.php(.*)\">/U", $buf, $formaction);
-    preg_match("/<input type=\"hidden\" name=\"charset_test\" value=\"(.*)\" \/>/U", $buf, $chartest);
-    preg_match("/<input type=\"hidden\" name=\"fb_dtsg\" value=\"(.*)\" autocomplete=\"off\" \/>/U", $buf, $dtsg);
-    preg_match("/<input type=\"hidden\" name=\"post_form_id\" value=\"(.*)\" \/>/U", $buf, $formid);
-
-    curl_close($ch);
-    unset($buf);
-    unset($ch);
-
-    /**
-     * 3. UPDATE STATUS: Use previously collected form data to send a post query
-     * which will update our status.
-     */
-    $postdata = array(
-	'charset_test' => $chartest[1],
-	'fb_dtsg' => $dtsg[1],
-	'post_form_id' => $formid[1],
-	'status' => $status,
-	'update' => 'Share'
-    );
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_COOKIEFILE, $cookies);
-    curl_setopt($ch, CURLOPT_USERAGENT, $uagent);
-    curl_setopt($ch, CURLOPT_URL, 'http://m.facebook.com/a/home.php' . $formaction[1]);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, postify($postdata));
-
-    $buf = curl_exec($ch);
-
-    echo "[+] Sending POST data to: http://m.facebook.com/a/home.php" . html_entity_decode($formaction[1]) . "\n";
-    print_r($postdata);
-    echo "\n\n";
-
-    /**
-     * Something will probably appear in $buf if it fails :)
-     */
-    curl_close($ch);
-    if (strlen($buf) > 0) {
-	echo "[ERR] Status update response:$buf\n\n";
-    }
-
-    unset($buf);
-    unset($ch);
-    unset($postdata);
-
-    /**
-     * Logout, end funtion update and return.
-     */
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_COOKIEFILE, $cookies);
-    curl_setopt($ch, CURLOPT_USERAGENT, $uagent);
-    curl_setopt($ch, CURLOPT_URL, 'http://m.facebook.com/logout.php');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
-
-    curl_exec($ch);
-
-    curl_close($ch);
-    echo "[+] Logging out: http://m.facebook.com/logout.php\n\n";
-    unset($ch);
-
-    return 0;
+function parse_inputs($html) {
+    $dom = new DOMDocument;
+    @$dom->loadxml($html);
+    $inputs = $dom->getElementsByTagName('input');
+    return($inputs);
 }
 
+/*
+ * @return form action url
+ */
+
+function parse_action($html) {
+    $dom = new DOMDocument;
+    @$dom->loadxml($html);
+    $form_action = $dom->getElementsByTagName('form')->item(0)->getAttribute('action');
+    if (!strpos($form_action, "//")) {
+        $form_action = "https://m.facebook.com$form_action";
+    }
+    return($form_action);
+}
+
+function login() {
+    /*
+     * Grab login page and parameters
+     */
+    $loginpage = grab_home();
+    $form_action = parse_action($loginpage);
+    $inputs = parse_inputs($loginpage);
+    $post_params = "";
+    foreach ($inputs as $input) {
+        switch ($input->getAttribute('name')) {
+            case 'email':
+                $post_params .= 'email=' . urlencode($GLOBALS['email']) . '&';
+                break;
+            case 'pass':
+                $post_params .= 'pass=' . urlencode($GLOBALS['pass']) . '&';
+                break;
+            default:
+                $post_params .= $input->getAttribute('name') . '=' . urlencode($input->getAttribute('value')) . '&';
+        }
+    }
+    echo "[i] Using these login parameters: $post_params";
+    /*
+     * Login using previously collected form parameters
+     */
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_COOKIEJAR, $GLOBALS['cookies']);
+    curl_setopt($ch, CURLOPT_COOKIEFILE, $GLOBALS['cookies']);
+    curl_setopt($ch, CURLOPT_USERAGENT, $GLOBALS['uagent']);
+    curl_setopt($ch, CURLOPT_URL, $form_action);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_params);
+    $loggedin = curl_exec($ch);
+    if ($GLOBALS['debug']) {
+        echo $loggedin;
+    }
+    curl_close($ch);
+    /*
+     * Check if location checking is turned on or you have to verify location
+     */
+    if (strpos($loggedin, "machine_name") || strpos($loggedin, "/checkpoint/") || strpos($loggedin, "submit[Continue]")) {
+        echo "\n[i] Found a checkpoint...\n";
+        checkpoint($loggedin);
+        echo "\n[i] Checkpoints passed...\n";
+    }
+}
+
+/*
+ * pass checkpoints
+ */
+
+function checkpoint($html) {
+    $form_action = parse_action($html);
+    $inputs = parse_inputs($html);
+    $post_params = "";
+    foreach ($inputs as $input) {
+        switch ($input->getAttribute('name')) {
+            case "":
+                break;
+            case "submit[I don't recognize]":
+                break;
+            case "submit[Don't Save]":
+                break;
+            case "machine_name":
+                $post_params .= 'machine_name=' . urlencode($GLOBALS['device_name']) . '&';
+                break;
+            default:
+                $post_params .= $input->getAttribute('name') . '=' . urlencode($input->getAttribute('value')) . '&';
+        }
+    }
+    if ($GLOBALS['debug']) {
+        echo "\nCheckpoint form action: $form_action\n";
+        echo "\nCheckpoint post params: $post_params\n";
+    }
+    //Verify the machine
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_COOKIEJAR, $GLOBALS['cookies']);
+    curl_setopt($ch, CURLOPT_COOKIEFILE, $GLOBALS['cookies']);
+    curl_setopt($ch, CURLOPT_USERAGENT, $GLOBALS['uagent']);
+    curl_setopt($ch, CURLOPT_URL, $form_action);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_params);
+    $home = curl_exec($ch);
+    if ($GLOBALS['debug']) {
+        echo "This is fucking shit: -- $home --";
+    }
+    curl_close($ch);
+
+    if (strpos($home, "machine_name") || strpos($home, "/checkpoint/") || strpos($home, "submit[Continue]")) {
+        echo "\n[i] Solving another checkpoint...\n";
+        checkpoint($home);
+    }
+}
+
+/*
+ * grab and return the homepage
+ */
+
+function grab_home() {
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_COOKIEJAR, $GLOBALS['cookies']);
+    curl_setopt($ch, CURLOPT_COOKIEFILE, $GLOBALS['cookies']);
+    curl_setopt($ch, CURLOPT_USERAGENT, $GLOBALS['uagent']);
+    curl_setopt($ch, CURLOPT_URL, 'https://m.facebook.com/');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    $html = curl_exec($ch);
+    if ($GLOBALS['debug']) {
+        echo $html;
+    }
+    curl_close($ch);
+    return($html);
+}
+
+/*
+ * update facebook status
+ */
+
+function update($status) {
+    $html = grab_home();
+
+    $form_action = parse_action($html);
+    $inputs = parse_inputs($html);
+    $post_params = "status=$status&";
+    foreach ($inputs as $input) {
+        $post_params .= $input->getAttribute('name') . '=' . urlencode($input->getAttribute('value')) . '&';
+    }
+
+
+    if ($GLOBALS['debug']) {
+        echo "\nStatus update form action: $form_action\n";
+        echo "\nStatus update params: $post_params\n";
+    }
+    /*
+     * post the update
+     */
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_COOKIEJAR, $GLOBALS['cookies']);
+    curl_setopt($ch, CURLOPT_COOKIEFILE, $GLOBALS['cookies']);
+    curl_setopt($ch, CURLOPT_USERAGENT, $GLOBALS['uagent']);
+    curl_setopt($ch, CURLOPT_URL, $form_action);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_params);
+    $updated = curl_exec($ch);
+    if ($GLOBALS['debug']) {
+        echo $updated;
+    }
+    curl_close($ch);
+}
+
+function logout() {
+    $dom = new DOMDocument;
+    @$dom->loadxml(grab_home());
+    $links = $dom->getElementsByTagName('a');
+    foreach ($links as $link) {
+        if (strpos($link->getAttribute('href'), 'logout.php')) {
+            $logout = $link->getAttribute('href');
+            break;
+        }
+    }
+
+    $url = 'https://m.facebook.com' . $logout;
+    /*
+     * just logout lol
+     */
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_COOKIEJAR, $GLOBALS['cookies']);
+    curl_setopt($ch, CURLOPT_COOKIEFILE, $GLOBALS['cookies']);
+    curl_setopt($ch, CURLOPT_USERAGENT, $GLOBALS['uagent']);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    $loggedout = curl_exec($ch);
+    if ($GLOBALS['debug']) {
+        echo "\nLogout url = $url\n";
+        echo $loggedout;
+    }
+    curl_close($ch);
+    echo "\n[i] Logged out.\n";
+}
+
+login();
+update($GLOBALS['status']);
+logout();
+unlink($cookies);
 ?>
