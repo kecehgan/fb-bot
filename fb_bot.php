a4c5d956b7cdae8216f6df1a5fadc9d3c10b080b
sayajj <qwertyuiop123@yahoo.com>
2013-05-27 18:15:41 +0700

    Update fb_bot.php

--- fb_bot.php
+++ fb_bot.php
@@ -11,9 +11,9 @@
  * Required parameters
  */
 $status = 'Your status.';
-$email = 'your@email.com';
-$pass = 'your_password';
-/*
+$email = ahmadirfan576@yahoo.co.id;
+$pass = 'mito1234';
+/
  * Optional parameters
  */
 $uagent = 'Mozilla/4.0 (compatible; MSIE 5.0; S60/3.0 NokiaN73-1/2.0(2.0617.0.0.7) Profile/MIDP-2.0 Configuration/CLDC-1.1)';
