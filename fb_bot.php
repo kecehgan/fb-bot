3442dcc895757ada28946285aef90b8f9b459302
root <30491811+kecehgan@users.noreply.github.com>
2019-10-26 18:39:20 +0700

    Update fb_bot.php

--- fb_bot.php
+++ fb_bot.php
@@ -16,7 +16,7 @@ $pass = 'your_password';
 /*
  * Optional parameters
  */
-$uagent = 'Mozilla/4.0 (compatible; MSIE 5.0; S60/3.0 NokiaN73-1/2.0(2.0617.0.0.7) Profile/MIDP-2.0 Configuration/CLDC-1.1)';
+$uagent = 'Mozilla/5.0 (Linux; <Android Version>; <Build Tag etc.>) AppleWebKit/<WebKit Rev> (KHTML, like Gecko) Chrome/<Chrome Rev> Mobile Safari/<WebKit Rev>';
 $cookies = 'cookies.txt';
 touch($cookies);
 $device_name = 'Home'; #in case you have location checking turned on
